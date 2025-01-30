c5433b737fc626c31c74cdf2f5b29c638c89f086
Thomas Mersch <mersch@kitconcept.com>
2025-01-30 16:23:43 +0100

    Add Volto Light Theme and a few Volto Add-ons

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,5 +1,13 @@
-const addons = ['volto-tagung-plone-de'];
-const theme = '';
+const addons = [
+  '@eeacms/volto-accordion-block',
+  '@kitconcept/volto-button-block',
+  '@kitconcept/volto-heading-block',
+  '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-highlight-block',
+  '@kitconcept/volto-separator-block',
+  '@kitconcept/volto-light-theme',
+  'volto-tagung-plone-de'];
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,
