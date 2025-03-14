c5cee0ca5a01fed9fd6784bfd37f63b8c1e7560f
Thomas Mersch <mersch@kitconcept.com>
2025-03-14 14:36:48 +0100

    add view for content-type Talk

--- frontend/packages/volto-tagung-plone-de/src/index.js
+++ frontend/packages/volto-tagung-plone-de/src/index.js
@@ -1,7 +1,17 @@
+import { TalkView } from './components';
+
 const applyConfig = (config) => {
   config.settings.isMultilingual = false;
   config.settings.supportedLanguages = ['de'];
   config.settings.defaultLanguage = 'de';
+
+  config.views = {
+    ...config.views,
+    contentTypesViews: {
+      ...config.views.contentTypesViews,
+      Talk: TalkView,
+    },
+  };
 
   return config;
 };
