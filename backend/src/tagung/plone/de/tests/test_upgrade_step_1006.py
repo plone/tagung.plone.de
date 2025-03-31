6b75c83e5a31d68337c7c9dcd096cbecd74dd587
MrTango <md@derico.de>
2025-03-31 17:19:14 +0300

    add upgrade step load new Sponsor CT setting

---
+++ backend/src/tagung/plone/de/tests/test_upgrade_step_1006.py
@@ -0,0 +1,28 @@
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+# from tagung.plone.de.testing import TAGUNG_PLONE_DE_FUNCTIONAL_TESTING
+from tagung.plone.de.testing import TAGUNG_PLONE_DE_INTEGRATION_TESTING
+
+import unittest
+
+
+class UpgradeStepIntegrationTest(unittest.TestCase):
+    layer = TAGUNG_PLONE_DE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+    def test_upgrade_step(self):
+        # dummy, add tests here
+        self.assertTrue(True)
+
+
+# class UpgradeStepFunctionalTest(unittest.TestCase):
+#
+#     layer = TAGUNG_PLONE_DE_FUNCTIONAL_TESTING
+#
+#     def setUp(self):
+#         self.portal = self.layer['portal']
+#         setRoles(self.portal, TEST_USER_ID, ['Manager'])
