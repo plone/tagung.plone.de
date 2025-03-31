cfe3f04d88e2913fba315c739aad782c271066e9
MrTango <md@derico.de>
2025-03-31 17:27:34 +0300

    add depracation note for Browserlayer interface

--- backend/src/tagung/plone/de/interfaces.py
+++ backend/src/tagung/plone/de/interfaces.py
@@ -7,5 +7,6 @@ class ITagungPloneDeLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
+# XXX: deprecated, do not use this interface!!!
 class IBrowserLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
