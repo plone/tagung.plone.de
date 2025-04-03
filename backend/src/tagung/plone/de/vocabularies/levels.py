e971270a6ed677bc1ca516084aee419e278aa541
Thomas Mersch <mersch@kitconcept.com>
2025-04-03 11:55:59 +0200

    add vocabulary for sponsor type

---
+++ backend/src/tagung/plone/de/vocabularies/levels.py
@@ -0,0 +1,52 @@
+# from plone import api
+from plone.dexterity.interfaces import IDexterityContent
+from tagung.plone.de import _
+from zope.globalrequest import getRequest
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+class VocabItem:
+    def __init__(self, token, value):
+        self.token = token
+        self.value = value
+
+
+@implementer(IVocabularyFactory)
+class Levels:
+    """ """
+
+    def __call__(self, context):
+        # Just an example list of content for our vocabulary,
+        # this can be any static or dynamic data, a catalog result for example.
+        items = [
+            VocabItem("Platinum", _("Platinum")),
+            VocabItem("Gold", _("Gold")),
+            VocabItem("Silver", _("Silver")),
+            VocabItem("Bronze", _("Bronze")),
+            VocabItem("Organizer", _("Organizer")),
+        ]
+
+        # Fix context if you are using the vocabulary in DataGridField.
+        # See https://github.com/collective/collective.z3cform.datagridfield/issues/31:  # NOQA: 501
+        if not IDexterityContent.providedBy(context):
+            req = getRequest()
+            context = req.PARENTS[0]
+
+        # create a list of SimpleTerm items:
+        terms = []
+        for item in items:
+            terms.append(
+                SimpleTerm(
+                    value=item.token,
+                    token=str(item.token),
+                    title=item.value,
+                )
+            )
+        # Create a SimpleVocabulary from the terms list and return it:
+        return SimpleVocabulary(terms)
+
+
+LevelsFactory = Levels()
